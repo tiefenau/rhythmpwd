1897f7be6fa2c80ef439ab59866992e700835830
Christian Tiefenau <tiefenau@cs.uni-bonn.de>
2016-09-08 16:28:32 +0200

    features

--- database.sql
+++ database.sql
@@ -1,3 +1,8 @@
 create table options (id serial primary key, rhythm boolean, rule boolean);
 create table runs (id serial primary key, trainings integer, date timestamp default current_timestamp, uid integer, password text, options_id int CONSTRAINT f_ro REFERENCES options(id));
 create table inputs (id serial PRIMARY KEY, run_id integer CONSTRAINT f_ir REFERENCES runs(id),input int[], training boolean, pwd text, pwdtest text);
+
+insert into options (rhythm, rule) values (true, true);
+insert into options (rhythm, rule) values (true, false);
+insert into options (rhythm, rule) values (false, true);
+insert into options (rhythm, rule) values (false, false);
